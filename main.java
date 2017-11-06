240afff38b62e6b06f12309b7abb1c9d5d53a125
Andrea Gelsomino <andrea_gelsomino@hotmail.com>
2017-11-06 12:53:45 +0100

    update

--- main.java
+++ main.java
@@ -3,6 +3,10 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		System.out.println("Hello Andrea");
+		System.out.println("Hello Netmind");
+		
 
 	}
 
